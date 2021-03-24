d7bbb1dda55e74f57d53e83837cbf0c6514bbd76
Jose Ricardo Silveira <ricardusilveira@gmail.com>
2021-03-24 11:24:36 -0300

    Adicionado imagem gráfico violin

--- machine_learning_py/dealing_with_data_of_many_dimensions/machine_learning_dealing__with_data_of_many_dimensions.py
+++ machine_learning_py/dealing_with_data_of_many_dimensions/machine_learning_dealing__with_data_of_many_dimensions.py
@@ -10,11 +10,14 @@ Created on Mon Mar 22 19:20:06 2021
 # Download file git clone https://github.com/alura-cursos/...
 # reducao-dimensionalidade
 
+from sklearn.preprocessing import StandardScaler
 from sklearn.dummy import DummyClassifier
 import pandas as pd
 from numpy.random import seed
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 results_diagnostics = pd.read_csv('exames.csv')
 
@@ -56,3 +59,71 @@ print(
     f"\nOur classifier accuracy score is {classifier.score(test_x, test_y) * 100:.2f}%")
 print(
     f"\nOur classifier accuracy score is {dummy_classifier.score(test_x, test_y) * 100:.2f}%")
+
+
+# Now lets take a plot to see distribution between malign (M) and benign (B)
+# We will use violinplot
+# First we will evaluate about exam_1, so
+
+# data_plot = pd.concat([diagnostics_values, exams_values.iloc[:, :10]],
+# axis=1)
+# #formatting to plot in violinplot
+# data_plot = pd.melt(
+#     data_plot, id_vars="diagnostico", var_name="exames", value_name="valores"
+# )
+
+# but before we plot, we can inspect the data_plot and notice that Y values
+# are big and small, so we will standard it with StandardScaler. And do it
+# before create data_plot.
+# StandardScaler returns an numpy array, so we must create another DataFrame
+# before we create data_plot again
+
+
+standardizer = StandardScaler()
+standardizer.fit(exams_values)
+standardized_exams_values_array = standardizer.transform(exams_values)
+# Constructing a DataFrame again, standardized this time
+standardized_exams_values = pd.DataFrame(
+    data=standardized_exams_values_array,
+    columns=exams_values.keys()
+)
+
+plt.close('all')
+figure_size = (21, 10)
+figure_number = 1
+
+
+def violin_graph(
+        values,
+        start,
+        end,
+        figsize=figure_size,
+        nfig=figure_number
+):
+    # Now create our data_plot
+    data_plot = pd.concat(
+        [diagnostics_values, values.iloc[:, start:end]],
+        axis=1
+    )
+    # formatting to plot in violinplot
+    data_plot = pd.melt(
+        data_plot,
+        id_vars="diagnostico",
+        var_name="exames",
+        value_name="valores"
+    )
+    plt.figure(nfig, figsize)
+    sns.violinplot(
+        x="exames",
+        y="valores",
+        hue="diagnostico",
+        data=data_plot,
+        split=True
+    )
+    plt.xticks(rotation=90)
+
+
+# Plotting we notice some relevance exams in this interval. However, exame_4
+# shows a constant distribution, that adds no information, so we discard it
+# In adition, we create a function to plot the graph everytime we need
+violin_graph(standardized_exams_values, 21, 28)
