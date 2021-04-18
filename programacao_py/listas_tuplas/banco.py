bed30178529b579d01fc5ecedafce5567d49f793
José Ricardo Silveira <ricardusilveira@gmail.com>
2021-04-17 23:42:27 -0300

    Adicionado mais métodos de comparação

--- programacao_py/listas_tuplas/banco.py
+++ programacao_py/listas_tuplas/banco.py
@@ -6,7 +6,10 @@ Created on Sat Apr 17 20:03:27 2021
 @author: ricardo
 """
 
+from functools import total_ordering
 
+
+@total_ordering
 class ContaSalario:
     def __init__(self, codigo):
         self._codigo = codigo
@@ -16,5 +19,18 @@ class ContaSalario:
         if type(outra_conta) != ContaSalario:
             return False
 
-        return self._codigo == outra_conta.codigo and self._saldo ==\
-            outra_conta.saldo
+        return self._codigo == outra_conta._codigo and self._saldo ==\
+            outra_conta._saldo
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __lt__(self, outra_conta):  # Quando comparamos com sorted e afins
+
+        if self._saldo != outra_conta._saldo:  # O que queremos comparar
+            return self._saldo < outra_conta._saldo
+        else:
+            return self._codigo < outra_conta._codigo
+
+    def __str__(self):
+        return f"[>>Codigo {self._codigo}, Saldo {self._saldo}<<]"
--- programacao_py/listas_tuplas/principal_listas_tuplas.py
+++ programacao_py/listas_tuplas/principal_listas_tuplas.py
@@ -7,6 +7,8 @@ Created on Sat Apr 17 17:48:06 2021
 """
 
 
+from operator import attrgetter
+from banco import ContaSalario as cs
 idades = (13, 28, 64, 52, 34, 42)
 print(range(len(idades)))  # lazy class, só abre se for pedido
 print(list(range(len(idades))))  # a classe lista desempacota tudo, o mesmo
@@ -48,3 +50,93 @@ print(list(reversed(idades)))  # reversed retorna um iterador, precisamos
 lista_idades = list(idades)
 lista_idades.sort()
 lista_idades.sort(reverse=True)
+
+
+conta_jose = cs(18)
+conta_jose.deposita(450)
+
+conta_keith = cs(5)
+conta_keith.deposita(650)
+
+conta_gui = cs(12)
+conta_gui.deposita(700)
+
+contas = [conta_jose, conta_keith, conta_gui]
+"""Para objetos, precisamos deixar bem claro para o Python o que ele quer
+comparar. Se formos deixar essas contas em ordem, podemos usar a classe
+sorted, mas devemos especificar o que vamos comparar no objeto ContaSalario
+como fizemos com o método mágico __eq__.
+Vamos usar o atributo key da classe sorted. Podemos usá-las de diferentes
+maneiras. Uma delas é usar um método ou função que tomará o seu argumento
+o objeto de comparação
+"""
+
+
+def extrai_conta(conta):
+    return conta._saldo
+
+
+for conta in sorted(contas, key=extrai_conta):
+    print(conta)
+
+"""Esse procedimento funciona, mas não é uma boa prática extrairmos um
+atributo privado. Se ele é privado, queremos que ele não seja exposto.
+Podemos também usar o método attrgetter da classe operator"""
+
+for conta in sorted(contas, key=attrgetter('_saldo')):
+    print(conta)
+
+
+"""
+Apesar de funcionar como o primeiro, ainda estamos usando um atributo
+privado, algo que o criador não gostaria, pois há uma razão para estar
+oculto. Não mexendo nesses atributos fora da classe, não estaremos
+quebrando o encapsulamento.
+Se usarmos a classe sorted diretamente em contas sem mais nada,
+verificaremos que o operador < precisa ser definido. Novamente, o Python
+precisa saber o que vai comparar. O código, o saldo? etc
+Neste caso, devemos definir o operador < entre os objetos ContaSalario.
+Queremos ordenar pelo saldo.
+Redefinimos o método mágico __lt__ de lesser than"""
+
+for conta in sorted(contas):
+    print(conta)  # funciona também com conta_jose < conta_keith
+
+"""Porém, se adotarmos duas contas com saldos iguais, como proceder?
+Devemos atribuir uma segunda preferência. Podemos fazer tanto isso pelo
+método attrgetter('_saldo', '_codigo') quanto modificar o método mágico
+__lt__ e atribuir uma condição no caso de igualdade.
+Vamos testar"""
+
+conta_jose = cs(18)
+conta_jose.deposita(800)
+
+conta_keith = cs(5)
+conta_keith.deposita(800)
+
+conta_gui = cs(12)
+conta_gui.deposita(700)
+
+contas = [conta_jose, conta_keith, conta_gui]
+# acessando novamente atributos privados fora da classe. Não é uma boa
+# prática
+for conta in sorted(
+        contas, key=attrgetter('_saldo', '_codigo')
+):  # atribuiu uma ordem de classificação no caso de igualdade
+    print(conta)
+
+# com o método mágico alterado com condição de igualdade
+for conta in sorted(contas):
+    print(conta)  # funciona também com conta_jose < conta_keith
+
+"""Conseguimos também criar o operador de comparação == e < e por tabela o
+> (que é oposto de <). Entretanto, o operador <= não é definido
+automaticamente. Para isso devemos criar ele em particular ou usar
+a ferramenta total_ordering e usar o decorador em cima da classe que
+atribuímos os métodos mágicos __eq__ e __lt__ {ou __gt__}"""
+
+conta_jose < conta_gui
+conta_jose < conta_keith
+conta_jose == conta_jose
+conta_jose <= conta_jose
+conta_jose <= conta_keith
